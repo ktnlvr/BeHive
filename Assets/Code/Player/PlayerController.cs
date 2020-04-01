1385303f0d0e1977a5e53672efb939c64d31999c
youngman114 <artur.roos@mail.ru>
2020-04-01 13:22:03 +0300

    Prototype materials added & Player controller setup

---
+++ Assets/Code/Player/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu(" BeHive/Player/Player Controller", 1)]
+public class PlayerController : MonoBehaviour
+{
+    [Header("Strong Dependencies")]
+    [SerializeField]
+    private CharacterController controller;
+    [SerializeField]
+    private GameObject camera;
+    private Camera cam;
+
+    // Inner variables
+    private float xRotation = 0f;
+    public void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+        cam = camera.GetComponent<Camera>();
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    public void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -Rules.CameraClamp, Rules.CameraClamp);
+
+        transform.Rotate(Vector3.up * mouseX);
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
+}
